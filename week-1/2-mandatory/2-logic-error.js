a7d9fb94763513b455e3b637a2f4e4a78a950beb
carterd888 <62474051+carterd888@users.noreply.github.com>
2020-06-14 11:07:19 +0100

    solved logic errors

--- week-1/2-mandatory/2-logic-error.js
+++ week-1/2-mandatory/2-logic-error.js
@@ -1,17 +1,23 @@
 // The syntax for this function is valid but it has an error, find it and fix it.
 
 function trimWord(word) {
-  return wordtrim();
+  return word.trim();
 }
+
+trimWord("  CodeYourFuture ");
 
 function getWordLength(word) {
-  return "word".length()
+  return word.length;
 }
 
+getWordLength("A wild sentence appeared!");
+
 function multiply(a, b, c) {
-  a * b * c;
-  return;
+  
+  return a * b * c;
 }
+
+multiply(2,3,6);
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
