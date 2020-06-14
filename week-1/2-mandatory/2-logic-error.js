6e5327c09c59668e3a877716a6027fe5dfca13a7
hiba <hiba.moh@yahoo.com>
2020-06-14 06:03:33 +0100

    Logic errors fixed
    indented the code and took the comments off

--- week-1/2-mandatory/2-logic-error.js
+++ week-1/2-mandatory/2-logic-error.js
@@ -1,34 +1,32 @@
-// The syntax for this function is valid but it has an error, find it and fix it.
-
 function trimWord(word) {
-  return (word.trim());
+  return word.trim();
 }
 
 function getWordLength(word) {
-  return (word.length)
+  return word.length;
 }
 
 function multiply(a, b, c) {
-    return(a * b * c);
+  return a * b * c;
 }
-
-/* ======= TESTS - DO NOT MODIFY ===== 
-There are some Tests in this file that will help you work out if your code is working.
-
-To run these tests type `node 2-logic-error` into your terminal
-*/
 
 function test(test_name, expr) {
   let status;
   if (expr) {
-      status = "PASSED"
+    status = "PASSED";
   } else {
-      status = "FAILED"
+    status = "FAILED";
   }
 
-  console.log(`${test_name}: ${status}`)
+  console.log(`${test_name}: ${status}`);
 }
 
-test("fixed trimWord function", trimWord("  CodeYourFuture ") === "CodeYourFuture")
-test("fixed wordLength function", getWordLength("A wild sentence appeared!") === 25)
-test("fixed multiply function", multiply(2,3,6) === 36)
+test(
+  "fixed trimWord function",
+  trimWord("  CodeYourFuture ") === "CodeYourFuture"
+);
+test(
+  "fixed wordLength function",
+  getWordLength("A wild sentence appeared!") === 25
+);
+test("fixed multiply function", multiply(2, 3, 6) === 36);
