520f02ffa84e92028b871ec471c7eef2365ac0a6
Denis-Andrei <denispeptanariu@gmail.com>
2020-06-14 19:12:51 +0100

    3-extra COMPLETED

--- week-1/3-extra/1-currency-conversion.js
+++ week-1/3-extra/1-currency-conversion.js
@@ -5,7 +5,10 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(price) {
+  let priceUSD = price * 1.4;
+  return priceUSD
+}
 
 /*
   CURRENCY FORMATTING
@@ -16,7 +19,12 @@ function convertToUSD() {}
   Find a way to add 1% to all currency conversions (think about the DRY principle)
 */
 
-function convertToBRL() {}
+function convertToBRL(price) {
+   let priceBRL = price * 5.7;
+   let fee = (1/100) * priceBRL;
+   let totalPriceBRL = priceBRL + fee;
+   return totalPriceBRL;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
