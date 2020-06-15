1c5243dc0969dc7acefe706654f75154eb796e07
davibaweja <davibaweja@gmail.com>
2020-06-15 18:35:41 +0100

    V1.3
    commented the explanations of some functions as asked and wrote a function as indicated.

--- week-1/2-mandatory/3-function-output.js
+++ week-1/2-mandatory/3-function-output.js
@@ -1,9 +1,20 @@
 // Add comments to explain what this function does. You're meant to use Google!
+
+// The Math.random() function returns a floating-point, pseudo-random number in the range 0 to less than 1 
+// (inclusive of 0, but not 1) with approximately uniform distribution over that range — 
+// which you can then scale to your desired range
+// so this function will generate random numbers between 0 and less than 10.
 function getNumber() {
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+
+// The concat() method is used to join two or more strings.
+
+// concat method does not change the existing strings,
+// but returns a new string containing the text of the joined strings.
+// this function will concatenate w1 and w2 and return it.
 function s(w1, w2) {
   return w1.concat(w2);
 }
@@ -11,6 +22,10 @@ function s(w1, w2) {
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together
   // Look at the test case below to understand what to expect in return
+  gap=" ";
+  word1= firstWord.concat(gap); 
+  word2= secondWord.concat(gap);
+  return word1.concat(word2.concat(thirdWord));
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== 
