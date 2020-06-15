4a0a6789c955923139e625ace672f73357aba2ba
g1st <gintstan@gmail.com>
2020-06-15 11:18:28 +0100

    Extra exercises solutions

--- week-1/3-extra/2-piping.js
+++ week-1/3-extra/2-piping.js
@@ -16,26 +16,30 @@
   the final result to the variable goodCode
 */
 
-function add() {
-
+function add(num1, num2) {
+  return parseFloat((num1 + num2).toFixed(2));
 }
 
-function multiply() {
-
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function format() {
-
+function format(num) {
+  return `£${num}`;
 }
 
-const startingValue = 2
+const startingValue = 2;
 
 // Why can this code be seen as bad practice? Comment your answer.
-let badCode = 
+// this is hard to read/understand what's going on
+let badCode = format(multiply(add(startingValue, 10), 2));
 
 /* BETTER PRACTICE */
+const addTen = add(startingValue, 10);
+const multiplyByTwo = multiply(addTen, 2);
+const formattedResult = format(multiplyByTwo);
 
-let goodCode = 
+let goodCode = formattedResult;
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
@@ -46,17 +50,17 @@ To run these tests type `node 2-piping.js` into your terminal
 function test(test_name, expr) {
   let status;
   if (expr) {
-      status = "PASSED"
+    status = 'PASSED';
   } else {
-      status = "FAILED"
+    status = 'FAILED';
   }
 
-  console.log(`${test_name}: ${status}`)
+  console.log(`${test_name}: ${status}`);
 }
 
-test('add function - case 1 works', add(1,3) === 4)
-test('add function - case 2 works', add(2.4,5.3) === 7.7)
-test('multiply function works', multiply(2,3) === 6)
-test('format function works', format(16) === "£16")
-test('badCode variable correctly assigned', badCode === "£24")
-test('goodCode variable correctly assigned', goodCode === "£24")
+test('add function - case 1 works', add(1, 3) === 4);
+test('add function - case 2 works', add(2.4, 5.3) === 7.7);
+test('multiply function works', multiply(2, 3) === 6);
+test('format function works', format(16) === '£16');
+test('badCode variable correctly assigned', badCode === '£24');
+test('goodCode variable correctly assigned', goodCode === '£24');
