e0e8ecb1da44892ff9534b821984dde5719e22f6
osagiestar <okoedooc@yahoo.com>
2020-06-15 14:40:13 +0100

    first JS practice

--- week-1/1-exercises/C-variables/exercise.js
+++ week-1/1-exercises/C-variables/exercise.js
@@ -1,7 +1,28 @@
 // Start by creating a variable `greeting`
 
-var message = "This is a string";
-var messageType = typeof message;
+// var message = "This is a string";
+// var messageType = typeof message;
 
-console.log(message);
-console.log(messageType);
+// console.log(message);
+// console.log(messageType);
+
+let greet = "Hello, ";
+let message = "my name is Osagie ";
+let name = "Osagie";
+let extra = "and the length of my name is ";
+
+let fullMessage = greet + message;
+let fullMessage2 = fullMessage.length;
+let messageCase = fullMessage.toUpperCase();
+let nameLength = name.length;
+let combined = message + extra + fullMessage2;
+
+console.log(fullMessage);
+
+console.log(fullMessage2);
+
+console.log(messageCase);
+
+console.log(nameLength);
+
+console.log(combined);
