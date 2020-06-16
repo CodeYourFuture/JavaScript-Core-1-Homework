88ce31d81717e9168f43af04689ba7529e322584
osagiestar <okoedooc@yahoo.com>
2020-06-16 15:30:56 +0100

    K-functions exercise2 answer no1

--- week-1/1-exercises/K-functions-parameters/exercise.js
+++ week-1/1-exercises/K-functions-parameters/exercise.js
@@ -1,7 +1,9 @@
+/*This question interpreted as two numbers, when divided, will give 0.75 as result*/
 // Complete the function so that it takes input parameters
-function multiply(num1, num2) {
-  return num1 * num2;
+
+function divide(num1, num2) {
+  return (num1 + num2) / 4;
 }
 // Assign the result of calling the function the variable `result`
-let result = multiply(3, 4);
+let result = divide(2, 1);
 console.log(result);
