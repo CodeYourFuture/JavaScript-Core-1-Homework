a05a8b9d54f32a2be8fbb6401ac98ad94e6fc0e8
osagiestar <okoedooc@yahoo.com>
2020-06-16 01:09:40 +0100

    I-floatss exercise

--- week-1/1-exercises/I-floats/exercise.js
+++ week-1/1-exercises/I-floats/exercise.js
@@ -1,2 +1,18 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+let totalofStudentsAndMentors = numberOfStudents + numberOfMentors;
+
+let percentOfStudents = (numberOfStudents / totalofStudentsAndMentors) * 100;
+let percentOfMentors = (numberOfMentors / totalofStudentsAndMentors) * 100;
+
+let approxPercent = Math.round(percentOfStudents);
+let approxPer = Math.round(percentOfMentors);
+let comment = "Percentage students :";
+
+console.log(percentOfStudents);
+console.log(percentOfMentors);
+
+console.log(approxPercent);
+console.log(approxPer);
+
+console.log(`${comment} ${approxPercent}`);
