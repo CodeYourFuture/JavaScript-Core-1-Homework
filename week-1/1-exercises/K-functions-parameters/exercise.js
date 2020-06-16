fc834ac3c5c1ac9c18fe555c03a0d74f2bf23152
osagiestar <okoedooc@yahoo.com>
2020-06-16 15:34:53 +0100

    K-functions 1-exercise// Complete the function so that it takes input parametersanswer no2

--- week-1/1-exercises/K-functions-parameters/exercise.js
+++ week-1/1-exercises/K-functions-parameters/exercise.js
@@ -1,9 +1,9 @@
-/*This question interpreted as two numbers, when divided, will give 0.75 as result*/
-// Complete the function so that it takes input parameters
+/*This question is interpreted as two numbers, one dividing the other as below*/
+//Complete the function so that it takes input parameters
 
 function divide(num1, num2) {
-  return (num1 + num2) / 4;
+  return num1 / num2;
 }
 // Assign the result of calling the function the variable `result`
-let result = divide(2, 1);
-console.log(result);
+let output = divide(3, 4);
+console.log(output);
