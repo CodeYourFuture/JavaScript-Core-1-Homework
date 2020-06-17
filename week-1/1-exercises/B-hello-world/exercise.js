0e0ba67435ddbc4f23470db9feddd43a29021158
Jacques Vertommen <jacquesvertommen@gmail.com>
2020-06-17 17:37:54 +0100

    1-syntax-error debugging

--- week-1/1-exercises/B-hello-world/exercise.js
+++ week-1/1-exercises/B-hello-world/exercise.js
@@ -1,1 +1,7 @@
 console.log("Hello world");
+console.log("Hello");
+console.log(45);
+console.log("Hello");
+
+// When you get rid of the quote marks you have a referenceError is not defined
+//when you console.log() just a number without quotes in this case the number going to display
