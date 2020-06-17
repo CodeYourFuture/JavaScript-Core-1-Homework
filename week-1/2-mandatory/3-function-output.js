b55ae4a1cf5ae1cc4d03a1c0868eadab92183084
zubeda <zubedauk@gmail.com>
2020-06-17 14:00:18 +0100

    3-function-output.js is done

--- week-1/2-mandatory/3-function-output.js
+++ week-1/2-mandatory/3-function-output.js
@@ -1,16 +1,17 @@
 // Add comments to explain what this function does. You're meant to use Google!
-function getNumber() {
+function getNumber() {//MAth.random()*10 return random numbers between 1 and 10
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
-function s(w1, w2) {
+function s(w1, w2) {//w1.concat(w2) return a new string containing the text of joined string
   return w1.concat(w2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
-  // Write the body of this function to concatenate three words together
-  // Look at the test case below to understand what to expect in return
+  
+  console.log(firstWord.concat(" ").concat(secondWord).concat(" ").concat(thirdWord));
+  return firstWord.concat(" ").concat(secondWord).concat(" ").concat(thirdWord);
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== 
@@ -34,10 +35,12 @@ test(
   "concatenate function - case 1 works",
   concatenate("code", "your", "future") === "code your future"
 );
+
 test(
   "concatenate function - case 2 works",
   concatenate("I", "like", "pizza") === "I like pizza"
 );
+
 test(
   "concatenate function - case 3 works",
   concatenate("I", "am", 13) === "I am 13"
