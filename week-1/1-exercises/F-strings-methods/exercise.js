1a00e3c8247e8de28057986e26599aa3859c6959
HusseinAlsakkaf <64165255+HusseinAlsakkaf@users.noreply.github.com>
2020-06-18 14:58:32 +0100

    exercises completed

--- week-1/1-exercises/F-strings-methods/exercise.js
+++ week-1/1-exercises/F-strings-methods/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
+var name = "Hussein";
+var namelength = name.length;
+console.log("My name is Hussein and my name is " + name.length + " characters long" );
 
-console.log(message);
+
