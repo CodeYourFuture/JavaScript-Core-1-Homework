3e5b2e621d3e9d683f80096da24d8781a4aa11a6
Jacques Vertommen <jacquesvertommen@gmail.com>
2020-06-18 15:03:22 +0100

    exercise1 F-strings-methods

--- week-1/1-exercises/F-strings-methods/exercise.js
+++ week-1/1-exercises/F-strings-methods/exercise.js
@@ -1,3 +1,8 @@
 // Start by creating a variable `message`
+const name = "Daniel";
+const nameLength = name.length;
+
+var message =
+  "My name is " + name + " and my name is " + nameLength + " characters long";
 
 console.log(message);
--- week-1/1-exercises/F-strings-methods/exercise2.js
+++ week-1/1-exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,7 @@
-const name = " Daniel  ";
+const name = "Daniel";
+const nameLength = name.length;
+
+var message =
+  "My name is " + name + " and my name is " + nameLength + " characters long";
 
 console.log(message);
