3a0533c6f0b1356352c859ea0b176f5fb7e7917d
Jacques Vertommen <jacquesvertommen@gmail.com>
2020-06-18 15:09:08 +0100

    exercise2 F string methods

--- week-1/1-exercises/F-strings-methods/exercise2.js
+++ week-1/1-exercises/F-strings-methods/exercise2.js
@@ -1,7 +1,13 @@
-const name = "Daniel";
+const name = " Daniel  ";
 const nameLength = name.length;
-
+newName = name.trim();
 var message =
-  "My name is " + name + " and my name is " + nameLength + " characters long";
+  "My name is " +
+  newName +
+  " and my name is " +
+  nameLength +
+  " characters long";
 
 console.log(message);
+
+
