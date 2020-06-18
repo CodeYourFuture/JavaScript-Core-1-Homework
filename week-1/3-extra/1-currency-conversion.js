37f2173fd1c028af3823b112c8f988d6611a8564
Selina Hussain <61423748+selinahussain@users.noreply.github.com>
2020-06-18 20:12:21 +0100

    Extra: currency conversion and piping completed.

--- week-1/3-extra/1-currency-conversion.js
+++ week-1/3-extra/1-currency-conversion.js
@@ -5,7 +5,10 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(priceGBP) {
+  let priceToUSD = priceGBP * 1.4;
+  return priceToUSD;
+}
 
 /*
   CURRENCY FORMATTING
@@ -16,7 +19,12 @@ function convertToUSD() {}
   Find a way to add 1% to all currency conversions (think about the DRY principle)
 */
 
-function convertToBRL() {}
+function convertToBRL(priceGBP) {
+  let priceToBRL = priceGBP * 5.7;
+  let fee = priceToBRL * (1 / 100);
+  let finalPrice = priceToBRL + fee;
+  return finalPrice;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
