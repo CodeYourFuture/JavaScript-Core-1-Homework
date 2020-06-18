aefbddfd0b8a934eda218a68a1b0f7ad50382ba9
iggajewski <64140239+iggajewski@users.noreply.github.com>
2020-06-18 18:26:45 +0100

    Completed part two of the week 1 homework

--- week-1/2-mandatory/4-tax.js
+++ week-1/2-mandatory/4-tax.js
@@ -5,7 +5,9 @@
   Sales tax is 20% of the price of the product
 */
 
-function calculateSalesTax() {}
+function calculateSalesTax(price) {
+  return price * 1.2;
+}
 
 /*
   CURRENCY FORMATTING
@@ -17,7 +19,10 @@ function calculateSalesTax() {}
   Remember that the prices must include the sales tax (hint: you already wrote a function for this!)
 */
 
-function formatCurrency() {}
+function formatCurrency(value) {
+  value = calculateSalesTax(value).toFixed(2);
+  return `£${value}`;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
