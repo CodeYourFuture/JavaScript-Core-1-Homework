45f3087a0715b3025db2d632d4e8d1fd711c9d8f
zubeda <zubedauk@gmail.com>
2020-06-18 11:17:05 +0100

    3-magic-8-ball.js is done

--- week-1/3-extra/3-magic-8-ball.js
+++ week-1/3-extra/3-magic-8-ball.js
@@ -35,7 +35,7 @@ Better not tell you now.
 Cannot predict now.
 Concentrate and ask again.
 
-## Very negative
+## Negative
 Don't count on it.
 My reply is no.
 My sources say no.
@@ -45,17 +45,74 @@ Very doubtful.
 
 // This should log "The ball has shaken!"
 // and return the answer.
-function shakeBall() {}
+function shakeBall() {
 
+ logged="The ball has shaken!";
+ answers=["Very positive","Positive","Negative","Negative"]
+//
+ //answers[Math.floor(Math.random()*answers.length)]
+//console.log(Math.floor(Math.random()*4));
+ answer=Math.floor(Math.random()*answers.length)
+ return answer.toString();
+}
+//answers[Math.floor(Math.random()*answers.length)]
 // The answer should come from shaking the ball
-let answer;
+//let answer;
 
 // When checking the answer, we should tell someone if the answer is
 // - very positive
 // - positive
 // - negative
 // - very negative
-function checkAnswer() {}
+function checkAnswer() {
+ // console.log("ans from main"+answer);
+  let subAns;
+  
+//let no=shakeBall();
+//console.log("answer again from checkS"+no);
+if(answer==0){
+   // ");
+    subAns=["It is certain."
+      ,"It is decidedly so."
+      ,"Without a doubt"
+      ,"Yes - definitely."
+      ,"You may rely on it."];
+    console.log("very positive");
+    console.log(subAns[Math.floor(Math.random()*subAns.length)]);
+    return "very positive";
+}
+if(answer==1){
+  subAns=["As I see it, yes."
+    ,"Most likely."
+    ,"Outlook good."
+    ,"Yes."
+    ,"Signs point to yes."];
+  console.log("positive");
+  console.log(subAns[Math.floor(Math.random()*subAns.length)]);
+  return "positive";
+}
+if(answer==2){
+  subAns=["Reply hazy, try again."
+    ,"Ask again later."
+    ,"Better not tell you now."
+    ,"Cannot predict now."
+    ,"Concentrate and ask again."];
+  console.log("negative");
+  console.log(subAns[Math.floor(Math.random()*subAns.length)]);
+  return "negative";
+}
+if(answer==3){
+  subAns=["Don't count on it."
+  ,"My reply is no."
+  ,"My sources say no."
+  ,"Outlook not so good."
+  ,"Very doubtful."];
+  console.log("very nagative");
+  console.log(subAns[Math.floor(Math.random()*subAns.length)]);
+  return "very negative";
+}
+ 
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
@@ -89,6 +146,7 @@ function testAll() {
     `shakeBall logs "The ball has shaken!"`,
     logged === "The ball has shaken!"
   );
+  
   test(`shakeBall returns an string answer"`, typeof answer === "string");
   test(
     `checkAnswer returns the level of positivity"`,
