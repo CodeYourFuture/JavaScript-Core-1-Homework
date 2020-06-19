832fe6e6ab1932fbb84e6ce4715cfc33522c5dd6
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-06-19 18:13:07 +0100

    functions-nested is done.

--- week-1/1-exercises/L-functions-nested/exercise.js
+++ week-1/1-exercises/L-functions-nested/exercise.js
@@ -21,3 +21,11 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+
+var mentorsNames= ["Daniel","Irina", "Mimi","Rob", "Yohannes"];
+
+    for (var i=0; i < mentorsNames.length; i++){
+        console.log("HELLO " + (mentorsNames[i].toUpperCase()));
+    }  
+
