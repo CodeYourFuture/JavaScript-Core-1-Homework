870aa1c85cfc1b7eeac9d4f042a92bd4f2b5fc5b
bireworld <mail2berhane@gmail.com>
2020-06-19 23:20:00 +0100

    exercise done G-numbers

--- week-1/1-exercises/G-numbers/exercise.js
+++ week-1/1-exercises/G-numbers/exercise.js
@@ -1,1 +1,5 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+var sum = numberOfStudents + numberOfMentors;
+console.log("Total numnber of students and mentors:" + sum);
