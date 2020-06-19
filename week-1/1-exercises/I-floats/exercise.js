8373dd5b49af65a43a6181b5f06a4702e46b3ee0
bireworld <mail2berhane@gmail.com>
2020-06-19 23:42:14 +0100

    exercise done I-floats update

--- week-1/1-exercises/I-floats/exercise.js
+++ week-1/1-exercises/I-floats/exercise.js
@@ -2,6 +2,8 @@ var numberOfStudents = 15;
 var numberOfMentors = 8;
 var stuPercentile = numberOfStudents/23*100;
 var mentorpercentile = numberOfMentors/23*100;
+stuPercentile = Math.round(stuPercentile);
+mentorpercentile = Math.round(mentorpercentile);
 console.log("Percentage Students:" +stuPercentile + "%" );
 console.log("Percentage Mentors:" + mentorpercentile + "%");
 
