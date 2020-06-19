e839977ea247d5031cad61c223ffd30ade13bf07
Jacques Vertommen <jacquesvertommen@gmail.com>
2020-06-20 00:20:09 +0100

    pipping

--- week-1/3-extra/2-piping.js
+++ week-1/3-extra/2-piping.js
@@ -16,26 +16,28 @@
   the final result to the variable goodCode
 */
 
-function add() {
-
+function add(pipe1, pipe2) {
+  return pipe1 + pipe2;
 }
 
-function multiply() {
-
+function multiply(pipe1, pipe2) {
+  return pipe1 * pipe2;
 }
 
-function format() {
-
+function format(pound) {
+  return `£${pound}`;
 }
 
 const startingValue = 2
 
 // Why can this code be seen as bad practice? Comment your answer.
-let badCode = 
+let badCode = format(multiply(add(startingValue, 10).toFixed(2), 2)); //badCode has all codes mumbled up// 
 
 /* BETTER PRACTICE */
 
-let goodCode = 
+let sum = add(2, 10);
+let product = multiply(sum, 2);
+let goodCode = format(product);
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
