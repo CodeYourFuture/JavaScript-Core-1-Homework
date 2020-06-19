3e8e82cdd6cc7352d2a5e010136729366f16b939
bireworld <mail2berhane@gmail.com>
2020-06-19 23:39:44 +0100

    exercise done I-floats

--- week-1/1-exercises/I-floats/exercise.js
+++ week-1/1-exercises/I-floats/exercise.js
@@ -1,2 +1,7 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var stuPercentile = numberOfStudents/23*100;
+var mentorpercentile = numberOfMentors/23*100;
+console.log("Percentage Students:" +stuPercentile + "%" );
+console.log("Percentage Mentors:" + mentorpercentile + "%");
+
