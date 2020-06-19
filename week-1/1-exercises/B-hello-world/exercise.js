bfd8e6030c0fa784d2999697fb1baaf3d786a3d5
osagiestar <okoedooc@yahoo.com>
2020-06-19 07:39:35 +0100

    test the console.log() syntax

--- week-1/1-exercises/B-hello-world/exercise.js
+++ week-1/1-exercises/B-hello-world/exercise.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+/* console.log() with quotes, single or double, returns the message on the console*/
+// console.log("Hello world. I just started learning JavaScript!");
+// console.log("Hello world. I just started learning JavaScript!");
+// console.log("Hello world. I just started learning JavaScript!");
+
+/*Without the quote marks, I see an error message (SyntaxError: missing) */
+// console.log(Hello world. I just started learning JavaScript!);
+
+/* with just a number and no quotes the output is the number */
+console.log(4);
