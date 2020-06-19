ee7a87472e9b68e6d0f5aecae30bf735cd8f8940
osagiestar <okoedooc@yahoo.com>
2020-06-19 08:43:19 +0100

    create variable aand output result

--- week-1/1-exercises/C-variables/exercise.js
+++ week-1/1-exercises/C-variables/exercise.js
@@ -1,28 +1,7 @@
-// Start by creating a variable `greeting`
+/* * */
 
-// var message = "This is a string";
-// var messageType = typeof message;
+let greeting = "Hello world";
 
-// console.log(message);
-// console.log(messageType);
-
-let greet = "Hello, ";
-let message = "my name is Osagie ";
-let name = "Osagie";
-let extra = "and the length of my name is ";
-let fullMessage = greet + message;
-
-let fullMessage2 = fullMessage.length;
-let messageCase = fullMessage.toUpperCase();
-let nameLength = name.length;
-let combined = message + extra + nameLength;
-
-console.log(fullMessage);
-
-console.log(fullMessage2);
-
-console.log(messageCase);
-
-console.log(nameLength);
-
-console.log(combined);
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
