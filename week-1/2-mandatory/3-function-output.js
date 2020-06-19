d037e1a7e4a7bc5aabf6318d26758961aae4e956
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-06-19 11:45:23 +0100

    3-function-output is finished.

--- week-1/2-mandatory/3-function-output.js
+++ week-1/2-mandatory/3-function-output.js
@@ -1,9 +1,19 @@
 // Add comments to explain what this function does. You're meant to use Google!
-function getNumber() {
+
+/* 'Math.random()' function returns a random number from 0 to 1 (not including 1).
+ Here that random number will be multiplied with 10 and return out of the function.
+ So, the random numbers will be between 0 and 9.99999 but not 10. */
+
+ function getNumber() {
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+
+/*  The concat() method is used to join two or more arrays.This method does 
+not change the existing arrays, but returns a new array, containing the values 
+of the joined arrays. */
+
 function s(w1, w2) {
   return w1.concat(w2);
 }
@@ -11,6 +21,7 @@ function s(w1, w2) {
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together
   // Look at the test case below to understand what to expect in return
+  return `${firstWord} ${secondWord} ${thirdWord}`;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== 
