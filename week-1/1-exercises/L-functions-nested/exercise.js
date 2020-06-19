c31c0783a03d72322aedb74284ec7e46ef05136a
Lola Bello <mololabello@gmail.com>
2020-06-19 23:13:23 +0100

    exercise updated

--- week-1/1-exercises/L-functions-nested/exercise.js
+++ week-1/1-exercises/L-functions-nested/exercise.js
@@ -3,3 +3,15 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function upperCase(name) {
+   let result = name.toUpperCase();
+    return result;
+
+
+}
+console.log(upperCase(mentor1));
+console.log(upperCase(mentor2));
+console.log(upperCase(mentor3));
+console.log(upperCase(mentor4));
+console.log(upperCase(mentor5));
