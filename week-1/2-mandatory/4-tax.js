1b5342434fd265688bdd164df71496b0e744e796
MartinBoylan <63868146+MartinBoylan@users.noreply.github.com>
2020-06-19 15:02:07 +0100

    Week 1 JS Martin mandatory

--- week-1/2-mandatory/4-tax.js
+++ week-1/2-mandatory/4-tax.js
@@ -5,7 +5,9 @@
   Sales tax is 20% of the price of the product
 */
 
-function calculateSalesTax() {}
+function calculateSalesTax(gross) {
+  return gross * 1.2
+}
 
 /*
   CURRENCY FORMATTING
@@ -17,7 +19,11 @@ function calculateSalesTax() {}
   Remember that the prices must include the sales tax (hint: you already wrote a function for this!)
 */
 
-function formatCurrency() {}
+function formatCurrency(x) {
+  var net = x * 1.2;
+  var result = net.toFixed([2]);
+  return "£" + result;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
