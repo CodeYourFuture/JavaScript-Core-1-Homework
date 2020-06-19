ecf75a657414a8a4443c5d4ccb238e01c24f85ea
Abdennour Hachemi <nouri89@hotmail.com>
2020-06-19 23:06:22 +0100

    solving float exercise

--- week-1/1-exercises/I-floats/exercise.js
+++ week-1/1-exercises/I-floats/exercise.js
@@ -1,2 +1,20 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+/*    Using the variables provided in the exercise calculate the percentage of mentors and students in the group
+
+## Expected result
+
+```
+Percentage students: 65%
+Percentage mentors: 35%*/
+var numberOfStudents = 15;
+var numberOfStudents = 15;
+var studentsPercentage = Math.round(
+	(numberOfStudents / (numberOfStudents + numberOfMentors)) * 100
+);
+var numberOfMentors = 8;
+var mentorsPercentage = Math.round(
+	(numberOfMentors / (numberOfStudents + numberOfMentors)) * 100
+);
+console.log(`Percentage students: ${studentsPercentage}%`);
+console.log(`Percentage mentors: ${mentorsPercentage}%`);
