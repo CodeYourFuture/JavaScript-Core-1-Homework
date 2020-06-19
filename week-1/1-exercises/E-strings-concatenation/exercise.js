b9d74006f6b4abcc3cb7f2286ac0aca2e9580f86
Abdennour Hachemi <nouri89@hotmail.com>
2020-06-19 22:49:19 +0100

    solving exercise E

--- week-1/1-exercises/E-strings-concatenation/exercise.js
+++ week-1/1-exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,5 @@
-// Start by creating a variable `message`
+        let greeting = "Hello, my name is ";
+        let myName = "Abdennour";
+        let message = greeting + myName;
 
-console.log(message);
+        console.log(message);
