13af045a97dd2344cf3e0df77be1fccc0d7a8118
davibaweja <davibaweja@gmail.com>
2020-06-20 19:15:11 +0100

    Revert "V1.2"
    This reverts commit 5192d44bbbece076152059646e6cd445c61edea3.

--- week-1/2-mandatory/2-logic-error.js
+++ week-1/2-mandatory/2-logic-error.js
@@ -1,16 +1,16 @@
 // The syntax for this function is valid but it has an error, find it and fix it.
 
 function trimWord(word) {
-  return word;
+  return wordtrim();
 }
 
 function getWordLength(word) {
-  return word.length;
+  return "word".length()
 }
 
 function multiply(a, b, c) {
- 
-  return  a * b * c;
+  a * b * c;
+  return;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== 
@@ -30,6 +30,6 @@ function test(test_name, expr) {
   console.log(`${test_name}: ${status}`)
 }
 
-test("fixed trimWord function", trimWord("CodeYourFuture") === "CodeYourFuture")
+test("fixed trimWord function", trimWord("  CodeYourFuture ") === "CodeYourFuture")
 test("fixed wordLength function", getWordLength("A wild sentence appeared!") === 25)
 test("fixed multiply function", multiply(2,3,6) === 36)
