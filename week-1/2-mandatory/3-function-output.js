aa6f64e6bb4ffd476c769865775876ee07f4a80a
Amanul Islam <amanulislamnaheem@gmail.com>
2020-06-21 14:32:19 +0100

    Mandatory exercises complete

--- week-1/2-mandatory/3-function-output.js
+++ week-1/2-mandatory/3-function-output.js
@@ -1,9 +1,11 @@
 // Add comments to explain what this function does. You're meant to use Google!
+// Math.random() returns a random number between 0 (inclusive) and 1 (exclusive).
 function getNumber() {
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+// This function concatenates parameters passed through the function, to return a new string.
 function s(w1, w2) {
   return w1.concat(w2);
 }
@@ -11,6 +13,7 @@ function s(w1, w2) {
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together
   // Look at the test case below to understand what to expect in return
+  return `${firstWord} ${secondWord} ${thirdWord}`;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== 
