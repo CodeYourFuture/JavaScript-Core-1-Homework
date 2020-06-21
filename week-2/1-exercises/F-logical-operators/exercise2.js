902a6e7708c2cf2586653940c6b1ca5f3dad6013
Hadiyah <62774630+HadiyahL@users.noreply.github.com>
2020-06-21 20:56:48 +0100

    Exercises Done, began mandatory exercises

--- week-2/1-exercises/F-logical-operators/exercise2.js
+++ week-2/1-exercises/F-logical-operators/exercise2.js
@@ -5,13 +5,26 @@
   Update the code so that you get the expected result.
 */
 
-function isNegative() {}
+function isNegative(number) {
+  return number < 0;
+}
 
+function isBetween5and10(number) {
+  return number > 5 && number <= 10;
+}
+
+function isShortName(name) {
+  return name.length < 7;
+}
+
+function startsWithD(name) {
+  return name[0] === "D";
+}
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
 
-console.log("Is -10 is a negative number?", isNegative(-10));
+console.log("Is -10 a negative number?", isNegative(-10));
 console.log("Is 5 a negative number?", isNegative(5));
 console.log("Is 10 in the range 5-10?", isBetween5and10(10));
 console.log("Is Daniel a short name?", isShortName("Daniel"));
@@ -20,7 +33,7 @@ console.log("Does Daniel start with 'D'?", startsWithD("Daniel"));
 /* 
   EXPECTED RESULT
   ---------------
-  Is -10 is a negative number? true
+  Is -10 a negative number? true
   Is 5 a negative number? false
   Is 10 in the range 5-10? true
   Is Daniel a short name? true
