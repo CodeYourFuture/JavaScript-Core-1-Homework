40b2040548d3d70eee3439cd11811fd19985238f
davibaweja <davibaweja@gmail.com>
2020-06-21 09:36:13 +0100

    V2.3 week2
    completed the tasks in file
    3-playing-computer.js

--- week-2/2-mandatory/3-playing-computer.js
+++ week-2/2-mandatory/3-playing-computer.js
@@ -7,12 +7,24 @@
   Answer the following questions:
 
   1. This program throws an error. Why? (If you can't find it, try executing it).
+  => b is undefined, console.log(b) will give errors.
   2. Remove the line that throws the error.
   3. What is printed to the console?
-  4. How many times is "f1" called?
-  5. How many times is "f2" called?
-  6. What value does the "a" parameter take in the first "f1" call?
+    2
+    6
+    4
+    9
+    6
+    13
+    8
+  4. How many times is "f1" called? 
+    => f1 is called 2 times.
+  5. How many times is "f2" called? 
+    => f2 is called 3 times.
+  6. What value does the "a" parameter take in the first "f1" call? 
+    => a paramter has the value 1
   7. What is the value of the "a" outer variable when "f1" is called for the first time?
+    => outer a takes the value 8
 */
 
 let x = 2;
@@ -26,9 +38,9 @@ const f2 = function(a, b) {
   return a + b + x;
 };
 
-console.log(x);
-console.log(a);
-console.log(b);
+console.log(x); // 2
+console.log(a); // 6
+// console.log(b); // undefined
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
