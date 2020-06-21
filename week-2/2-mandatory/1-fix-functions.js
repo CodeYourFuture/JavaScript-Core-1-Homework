a2b02c7ef6df1dfa1b12a2274a8ae234173cf006
Sadat <sadat0akhtar@gmail.com>
2020-06-21 18:20:30 +0100

    completed first mandotary exercise homework

--- week-2/2-mandatory/1-fix-functions.js
+++ week-2/2-mandatory/1-fix-functions.js
@@ -4,17 +4,18 @@
 function mood() {
   let isHappy = true;
 
-  if (isHappy) {
+  if (isHappy !== true) {
     return "I am happy";
   } else {
     return "I am not happy";
   }
 }
 
+
 function greaterThan10() {
   let num = 10;
-  let isBigEnough;
-
+  let isBigEnough = num >= 10;
+  
   if (isBigEnough) {
     return "num is greater than or equal to 10";
   } else {
@@ -24,21 +25,21 @@ function greaterThan10() {
 
 function sortArray() {
   let letters = ["a", "n", "c", "e", "z", "f"];
-  let sortedLetters;
+  let sortedLetters = letters.sort();
 
   return sortedLetters;
 }
 
 function first5() {
   let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-  let sliced;
+  let sliced = numbers.slice(0,5);
 
   return sliced;
 }
 
 function get3rdIndex(arr) {
   let index = 3;
-  let element;
+  let element = arr[index];
 
   return element;
 }
