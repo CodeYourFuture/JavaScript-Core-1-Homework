3bc2b202549fe0dbdfa0961155536676310ec17b
g1st <gintstan@gmail.com>
2020-06-22 09:43:24 +0100

    Use bubble sort

--- week-2/2-mandatory/4-sorting-algorithm.js
+++ week-2/2-mandatory/4-sorting-algorithm.js
@@ -21,12 +21,13 @@ function sortAges(arr) {
     }
   }
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      let temp = arr[i];
-      arr[i] = arr[i + 1];
-      arr[i + 1] = temp;
-      i = -1;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
     }
   }
   return arr;
