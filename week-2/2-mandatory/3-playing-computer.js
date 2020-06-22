a63d8a4e5a41ab324411b7ea40ff9c25d707cf18
zubeda <zubedauk@gmail.com>
2020-06-22 15:27:43 +0100

    3-playing-computer.js is done

--- week-2/2-mandatory/3-playing-computer.js
+++ week-2/2-mandatory/3-playing-computer.js
@@ -7,16 +7,23 @@
   Answer the following questions:
 
   1. This program throws an error. Why? (If you can't find it, try executing it).
-  2. Remove the line that throws the error.
-  3. What is printed to the console?
-  4. How many times is "f1" called?
-  5. How many times is "f2" called?
+  //variable b is not defined
+  2. Remove the line that throws the error.//34
+  3. What is printed to the console?//
+  2,6,4,9,6,13,8
+  4. How many times is "f1" called?//
+      2 times
+  5. How many times is "f2" called?//
+      3 times
   6. What value does the "a" parameter take in the first "f1" call?
+        8
   7. What is the value of the "a" outer variable when "f1" is called for the first time?
+        8
 */
 
 let x = 2;
 let a = 6;
+
 
 const f1 = function(a, b) {
   return a + b;
@@ -26,17 +33,18 @@ const f2 = function(a, b) {
   return a + b + x;
 };
 
-console.log(x);
-console.log(a);
-console.log(b);
+console.log(x);//2
+console.log(a);//6
+//console.log(b);
 
-for (let i = 0; i < 5; ++i) {
-  a = a + 1;
-  if (i % 2 === 0) {
-    const d = f2(i, x);
-    console.log(d);
-  } else {
-    const e = f1(i, a);
-    console.log(e);
+for (let i = 0; i < 5; ++i) {//0//1//2//3//4
+  a = a + 1;//7//8//9//10//11
+  if (i % 2 === 0) {//0//2//4
+    const d = f2(i, x);//0,2//2,2//4,2
+    console.log(d);//4//6//8
+  } else {//1//3
+    const e = f1(i, a);//1,8//3,10
+    console.log(e);//9//13
   }
+  //4,9,6,13,8
 }
