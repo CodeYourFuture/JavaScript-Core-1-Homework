1d6f9ce2b123f651ae1b376d8a4278ab3f6a85d2
bireworld <mail2berhane@gmail.com>
2020-06-23 22:18:07 +0100

    conditionals done

--- week-2/1-exercises/E-conditionals/exercise.js
+++ week-2/1-exercises/E-conditionals/exercise.js
@@ -8,6 +8,11 @@
 
 var name = "Daniel";
 var danielsRole = "mentor";
+if (name === danielsRole) {
+  console.log("Hi, I'm Daniel, I'm a mentor.");
+} else {
+  console.log("Hi, I'm Daniel, I'm a student.");
+}
 
 /* 
 EXPECTED RESULT
