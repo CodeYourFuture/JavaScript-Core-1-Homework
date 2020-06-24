e88ad139209076c97914fe03d044526f2c4e52a0
Hadiyah <62774630+HadiyahL@users.noreply.github.com>
2020-06-24 23:40:12 +0100

    part 4 done
    Mandatory part 4 - sorting algorithm done

--- week-2/2-mandatory/3-playing-computer.js
+++ week-2/2-mandatory/3-playing-computer.js
@@ -6,37 +6,37 @@
 
   Answer the following questions:
 
-  1. This program throws an error. Why? (If you can't find it, try executing it).
-  2. Remove the line that throws the error.
+  1. This program throws an error. Why? (If you can't find it, try executing it). b IS UNDEFINED
+  2. Remove the line that throws the error.   LINE 20 REMOVED
   3. What is printed to the console?
-  4. How many times is "f1" called?
-  5. How many times is "f2" called?
-  6. What value does the "a" parameter take in the first "f1" call?
-  7. What is the value of the "a" outer variable when "f1" is called for the first time?
+  4. How many times is "f1" called?  3 TIMES
+  5. How many times is "f2" called?  TWICE
+  6. What value does the "a" parameter take in the first "f1" call? 7
+  7. What is the value of the "a" outer variable when "f1" is called for the first time? 6
 */
 
 let x = 2;
 let a = 6;
 
-const f1 = function(a, b) {
+const f1 = function (a, b) {
   return a + b;
 };
 
-const f2 = function(a, b) {
+const f2 = function (a, b) {
   return a + b + x;
 };
 
 console.log(x);
 console.log(a);
-console.log(b);
+//console.log(b);
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
   if (i % 2 === 0) {
     const d = f2(i, x);
-    console.log(d);
+    console.log("This is the " + i + "th iteration of f2 = " + d);
   } else {
     const e = f1(i, a);
-    console.log(e);
+    console.log("This is the " + i + "th iteration of f1 = " + e);
   }
 }
