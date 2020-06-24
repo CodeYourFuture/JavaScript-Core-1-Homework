02fb013df542044ad7a823dfe6d3f1f94a10bded
HusseinAlsakkaf <64165255+HusseinAlsakkaf@users.noreply.github.com>
2020-06-24 14:24:47 +0100

    Mandatory completed

--- week-2/2-mandatory/4-sorting-algorithm.js
+++ week-2/2-mandatory/4-sorting-algorithm.js
@@ -14,7 +14,55 @@ You don't have to worry about making this algorithm work fast! The idea is to ge
 "think" like a computer and practice your knowledge of basic JavaScript.
 */
 
-function sortAges(arr) {}
+ 
+ function sortAges(arr) {
+  var newArray = [];
+   for(i = 0; i < arr.length; i++){
+
+    if(typeof arr[i] === "number"){
+      newArray.push(arr[i]);
+    } 
+    
+   }
+  
+   // **sorting** 
+   
+let sort = arry =>{
+  const arry2 = [];
+  let pos = 0, max = newArray[0], min = newArray[0];
+  
+  // max number
+  for(let j=0; j < newArray.length; j++){
+    if(max < newArray[j]){
+      max = newArray[j];
+    } 
+  }
+  // min loops
+  
+  for(let z = 0; z < newArray.length; z++){
+    for(let k =0; k< newArray.length; k++){
+      if(newArray[k] !== null){
+        if(min > newArray[k]){
+          min = newArray[k];
+          pos = k;
+        }
+      }
+    
+    }
+    arry2[z] = min;
+    newArray[pos] = null;
+    min = max;
+  }
+  return arry2;
+  
+  }
+return (sort(newArray));
+
+ }
+
+
+
+
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
