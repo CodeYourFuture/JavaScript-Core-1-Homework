72f97b04ec20cd9b3b2a080b65740d2a0066a6cc
Abdennour Hachemi <nouri89@hotmail.com>
2020-06-25 19:18:16 +0100

    solving sorting-algorithm exercise

--- week-2/2-mandatory/4-sorting-algorithm.js
+++ week-2/2-mandatory/4-sorting-algorithm.js
@@ -14,57 +14,58 @@ You don't have to worry about making this algorithm work fast! The idea is to ge
 "think" like a computer and practice your knowledge of basic JavaScript.
 */
 
-function sortAges(arr) {}
+function sortAges(arr) {
+	
+	var ArrayOfNumbers = [];
+	for (var i = 0; i < arr.length; i++) {
+		if (typeof arr[i] === 'number') {
+			ArrayOfNumbers.push(arr[i]);
+		}
+	}
+	
+	arr = ArrayOfNumbers;
+	var temp;
+	for (var k = 0; k < arr.length; k++) {
+		for (var j = k + 1; j < arr.length; j++) {
+			if (arr[k] > arr[j]) {
+				temp = arr[k];
+				arr[k] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+
+	return arr;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
-const agesCase1 = [
-  "🎹",
-  100,
-  "💩",
-  55,
-  "🥵",
-  "🙈",
-  45,
-  "🍕",
-  "Sanyia",
-  66,
-  "James",
-  23,
-  "🎖",
-  "Ismeal",
-];
-const agesCase2 = ["28", 100, 60, 55, "75", "🍕", "Elamin"];
+const agesCase1 = [ '🎹', 100, '💩', 55, '🥵', '🙈', 45, '🍕', 'Sanyia', 66, 'James', 23, '🎖', 'Ismeal' ];
+const agesCase2 = [ '28', 100, 60, 55, '75', '🍕', 'Elamin' ];
 
 function arraysEqual(a, b) {
-  if (a === b) return true;
-  if (a == null || b == null) return false;
-  if (a.length != b.length) return false;
+	if (a === b) return true;
+	if (a == null || b == null) return false;
+	if (a.length != b.length) return false;
 
-  for (let i = 0; i < a.length; ++i) {
-    if (a[i] !== b[i]) return false;
-  }
+	for (let i = 0; i < a.length; ++i) {
+		if (a[i] !== b[i]) return false;
+	}
 
-  return true;
+	return true;
 }
 
 function test(test_name, expr) {
-  let status;
-  if (expr) {
-    status = "PASSED";
-  } else {
-    status = "FAILED";
-  }
+	let status;
+	if (expr) {
+		status = 'PASSED';
+	} else {
+		status = 'FAILED';
+	}
 
-  console.log(`${test_name}: ${status}`);
+	console.log(`${test_name}: ${status}`);
 }
 
-test(
-  "sortAges function works - case 1",
-  arraysEqual(sortAges(agesCase1), [23, 45, 55, 66, 100])
-);
+test('sortAges function works - case 1', arraysEqual(sortAges(agesCase1), [ 23, 45, 55, 66, 100 ]));
 
-test(
-  "sortAges function works - case 2",
-  arraysEqual(sortAges(agesCase2), [55, 60, 100])
-);
+test('sortAges function works - case 2', arraysEqual(sortAges(agesCase2), [ 55, 60, 100 ]));
