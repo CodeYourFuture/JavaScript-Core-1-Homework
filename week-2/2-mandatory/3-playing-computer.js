9e9acbfa04912f6510991d53ab0da4acdc5bf4c8
Abdennour Hachemi <nouri89@hotmail.com>
2020-06-25 19:16:24 +0100

    solve plying-computer exercise

--- week-2/2-mandatory/3-playing-computer.js
+++ week-2/2-mandatory/3-playing-computer.js
@@ -15,28 +15,39 @@
   7. What is the value of the "a" outer variable when "f1" is called for the first time?
 */
 
+//Answers:
+// 1. Varible b is not defined which cause an error to be thrown.
+// 2. Line 40 has been removed.
+// 3. The console will print 2 6 4 9 6 13 8.
+// 4. f1  will be executed   2 times,  when  the loop counter i has odd value of 1 and 3.
+// 5. f2  will be executed   3 times  when  the loop counter i has even value of 0,2 and 4.
+// 6. The "a" parameter take in the first "f1" call the value of i which is 1.
+// 7. The value "a" outer variable when "f1" is called for the first time is 8 ?
+
 let x = 2;
 let a = 6;
 
 const f1 = function(a, b) {
-  return a + b;
+	return a + b;
 };
 
+
 const f2 = function(a, b) {
-  return a + b + x;
+	return a + b + x;
 };
 
 console.log(x);
 console.log(a);
-console.log(b);
+//console.log(b);
 
 for (let i = 0; i < 5; ++i) {
-  a = a + 1;
-  if (i % 2 === 0) {
-    const d = f2(i, x);
-    console.log(d);
-  } else {
-    const e = f1(i, a);
-    console.log(e);
-  }
+	a = a + 1;
+	if (i % 2 === 0) {
+		// if i is an even number call f2:it will be executed when i =0,2,4 .
+		const d = f2(i, x);
+		console.log(d);
+	} else {
+		const e = f1(i, a); // if i is an odd number call f1: it will be executed when i =1,3 .
+		console.log(e);
+	}
 }
