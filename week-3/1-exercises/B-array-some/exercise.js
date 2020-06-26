59907cab1e4e1b3539b7cf4ad15a6060a60a5711
NihalAli2 <62475392+NihalAli2@users.noreply.github.com>
2020-06-26 22:38:36 +0100

    Exercise-array-some

--- week-3/1-exercises/B-array-some/exercise.js
+++ week-3/1-exercises/B-array-some/exercise.js
@@ -12,12 +12,17 @@ var pairsByIndex = [[0, 3], [1, 2], [2, 1], null, [3, 0]];
 // https://nodejs.org/api/process.html#process_process_exit_code
 // process.exit(1);
 
+
 var students = ["Islam", "Lesley", "Harun", "Rukmini"];
 var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
 
 var pairs = pairsByIndex.map(function(indexes) {
+  if (indexes === null) {
+    process.exit(1);
+  }
   var student = students[indexes[0]];
   var mentor = mentors[indexes[1]];
+  
   return [student, mentor];
 });
 
