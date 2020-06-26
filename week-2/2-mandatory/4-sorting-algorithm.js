4b4e3c69c9f8d1891c364001e01c275725da943f
Sadat <sadat0akhtar@gmail.com>
2020-06-26 16:17:16 +0100

    completed sort algorythm successfully

--- week-2/2-mandatory/4-sorting-algorithm.js
+++ week-2/2-mandatory/4-sorting-algorithm.js
@@ -14,41 +14,54 @@ You don't have to worry about making this algorithm work fast! The idea is to ge
 "think" like a computer and practice your knowledge of basic JavaScript.
 */
 
-let myArray = ["helen",500, 300, "🍕",99, "peter",200, "joe",100 ];
+
+
 
 //THIS FUNCTION REMOVES ANY NON NUMBER VALUES FROM A GIVEN ARRAY.
+
 function numberChecker(arr){
-
-    for(let z=0; z< arr.length; z++){
-
-        if(typeof(arr[z]) == 'number'){
-          //console.log(z);
+    let emptyArray = [];
+    for(let z=0; z < arr.length; z++){
+       
+        if(typeof(arr[z]) === 'number'){
+            emptyArray.push(arr[z]);
         }else{
-            arr.splice(z, 1);
+            //remove selected index
+           // arr.splice(z, 1);
         }
+    
     
        
     }
-    return arr;
+    return emptyArray;
 }
 
 function sortAges(arr){
     let swap;
     let done = false;
     let changed;
+    let arr1 = numberChecker(arr);
 
     while(!done){
             changed = 0;
 
-            for(let i=1; i < arr.length; i++){
+            for(let i=1; i < arr1.length+1; i++){
 
-                if(arr[i-1] > arr[i]){
-                    swap = arr[i]; //swap equals 100
-                    arr[i] = arr[i-1]; //200 in index 1
-                    arr[i-1] = swap; //index zero equals 100(swap)
-                    changed = 1;     
+                if(arr1[i-1] > arr1[i]){
+                    swap = arr1[i-1];
+                    arr1[i-1] = arr1[i];
+                    arr1[i] = swap;
+                    changed = 1;
 
                 };
+
+                // if(arr1[i-1] > arr1[i]){
+                //     swap = arr1[i]; //swap equals 100
+                //     arr1[i] = arr1[i-1]; //200 in index 1
+                //     arr1[i-1] = swap; //index zero equals 100(swap)
+                //     changed = 1;     
+
+                // };
 
                 //USED TO EXIT THE WHILE LOOP
                 if(changed === 0){
@@ -57,63 +70,73 @@ function sortAges(arr){
                 }
            }
     }
+                //TO COUNTER THE FOUND BUG I HAD TO RUN THIS CODE SNIPPET ONE MORE TIME
+                if(arr1[0] > arr1[1]){
+                    swap = arr1[0];
+                    arr1[0] = arr1[1];
+                    arr1[1] = swap;
+                
 
-    return arr;
+                };
+    return arr1;
+
 }
 
 //USED TO TEST THE sortAges FUNCTION SEPERATELY (IT WORKS FINE HERE BUT NOT WITH THE TEST CODE BELOW)
-console.log(sortAges(numberChecker(myArray)));
+//console.log(sortAges(myArray));
+
+
 
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
-// const agesCase1 = [
-//   "🎹",
-//   100,
-//   "💩",
-//   55,
-//   "🥵",
-//   "🙈",
-//   45,
-//   "🍕",
-//   "Sanyia",
-//   66,
-//   "James",
-//   23,
-//   "🎖",
-//   "Ismeal",
-// ];
-// const agesCase2 = ["28", 100, 60, 55, "75", "🍕", "Elamin"];
+const agesCase1 = [
+  "🎹",
+  100,
+  "💩",
+  55,
+  "🥵",
+  "🙈",
+  45,
+  "🍕",
+  "Sanyia",
+  66,
+  "James",
+  23,
+  "🎖",
+  "Ismeal",
+];
+const agesCase2 = ["28", 100, 60, 55, "75", "🍕", "Elamin"];
 
-// function arraysEqual(a, b) {
-//   if (a === b) return true;
-//   if (a == null || b == null) return false;
-//   if (a.length != b.length) return false;
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
 
-//   for (let i = 0; i < a.length; ++i) {
-//     if (a[i] !== b[i]) return false;
-//   }
+  for (let i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
 
-//   return true;
-// }
+  return true;
+}
 
-// function test(test_name, expr) {
-//   let status;
-//   if (expr) {
-//     status = "PASSED";
-//   } else {
-//     status = "FAILED";
-//   }
+function test(test_name, expr) {
+  let status;
+  if (expr) {
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
 
-//   console.log(`${test_name}: ${status}`);
-// }
+  console.log(`${test_name}: ${status}`);
+}
 
-// test(
-//   "sortAges function works - case 1",
-//   arraysEqual(sortAges(agesCase1), [23, 45, 55, 66, 100])
-// );
+test(
+  "sortAges function works - case 1",
+  arraysEqual(sortAges(agesCase1), [23, 45, 55, 66, 100])
+);
 
-// test(
-//   "sortAges function works - case 2",
-//   arraysEqual(sortAges(agesCase2), [55, 60, 100])
-// );
+test(
+  "sortAges function works - case 2",
+  arraysEqual(sortAges(agesCase2), [55, 60, 100])
+);
