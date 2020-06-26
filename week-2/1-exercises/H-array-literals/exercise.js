d044df6cf04eeff86e5fb374780a8539baa0f690
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-06-26 12:02:04 +0100

    update exercise
    fixed one array

--- week-2/1-exercises/H-array-literals/exercise.js
+++ week-2/1-exercises/H-array-literals/exercise.js
@@ -4,7 +4,7 @@
   Declare some variables assigned to arrays of values
 */
 
-var numbers = [1,2,3,6,7]; // add numbers from 1 to 10 into this array
+var numbers = [1,2,3,4,5,6,7,8,9,10]; // add numbers from 1 to 10 into this array
 var mentors; // Create an array with the names of the mentors: Daniel, Irina and Rares
 var mentors = ['Daniel', 'Irina', 'Rares'];
 /* 
