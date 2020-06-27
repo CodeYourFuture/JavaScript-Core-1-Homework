c1908fdc6bfe7bf226adde095bbf3c939f2b173c
Hiba-moh <Hiba.moh@yahoo.com>
2020-06-27 09:31:53 +0100

    Exercise-D-array-Done

--- week-3/1-exercises/D-array-filter/exercise.js
+++ week-3/1-exercises/D-array-filter/exercise.js
@@ -1,14 +1,17 @@
 /*
   You are given a program that logs pairings between mentors and students
-  It fails because the array `pairsById` can contain different values that break the program
+  It fails because the array `pairsById` can contain different values
+   that break the program
   It is decided that array items which are not pairs should be filtered out
   - Finish the statement on line 11 to produce an array with valid content
   - Do not edit any of the existing code
 */
 
 var pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
-
-var pairsByIndex; // Complete this statement
+function pair(arr){
+  return (Array.isArray(arr) && arr.length === 2)  
+}
+var pairsByIndex=pairsByIndexRaw.filter(pair); // Complete this statement
 
 var students = ["Islam", "Lesley", "Harun", "Rukmini"];
 var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
