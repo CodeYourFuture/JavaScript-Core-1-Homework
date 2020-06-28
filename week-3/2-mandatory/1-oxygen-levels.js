fc57781fdd4adfe39794c282684baf8a955564c1
Hiba-moh <Hiba.moh@yahoo.com>
2020-06-28 05:00:13 +0100

    Mandatory-1-Oxygen-levels-Done

--- week-3/2-mandatory/1-oxygen-levels.js
+++ week-3/2-mandatory/1-oxygen-levels.js
@@ -9,8 +9,12 @@ To be safe, they need to land on the first unamed planet that has Oxygen levels 
 Write a function that finds the oxygen level of the first safe planet - Oxygen between 19.5% and 23.5%
 */
 
-function safeLevels() {
+function oxygen(oxygenLevel){
+  return (oxygenLevel>"19.5%" && oxygenLevel<"23.5%")    
+}
 
+function safeLevels(arr) {
+  return (arr.find(oxygen))
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
