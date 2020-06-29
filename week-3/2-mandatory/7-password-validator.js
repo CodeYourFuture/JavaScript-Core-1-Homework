f46a4a2354f510ee754f63d19e1a7e4287e4eb76
davibaweja <davibaweja@gmail.com>
2020-06-29 23:04:07 +0100

    V3.7 week3
    Completed the tasks in file 7-password-validator.js

--- week-3/2-mandatory/7-password-validator.js
+++ week-3/2-mandatory/7-password-validator.js
@@ -22,8 +22,87 @@ PasswordValidationResult=  [false, false, false, false, true]
 
 */
 
-function validatePasswords(passwords) {
+//-----------------------------------------Explanation of the parameters.----------------
+// passw is the password
+// passwords is the array
+// isFirstTime is a boolean for the passw. if it is true it means that => passw is not repeated in the array
+// else it is repeated more than once in the array.
+function check(passw,passwords,isFirstTime)
+{
+  
+  if( passw.length>=5     && 
+      hasNumbers(passw)   &&  
+      hasLowerCase(passw) && 
+      hasUpperCase(passw) && 
+      hasChars(passw)     && 
+      isFirstTime
+    )
+  {
+    return true;
+  }
 
+  return false;
+  
+}
+
+function hasNumbers(pass) // This function will check if a string has a number.
+{
+var regex = /\d/g;
+return regex.test(pass);
+} 
+
+
+function hasLowerCase(str) {
+  return (/[a-z]/.test(str));
+}
+function hasUpperCase(str) {
+  return (/[A-Z]/.test(str));
+}
+
+// this function checks if the pass argument has a series of characters.
+function hasChars(pass)
+{
+  var value_1 = /!|#|%/.test(pass);
+  var value_2=pass.includes(".");
+  var value_3=pass.includes("$");
+
+  if(value_1 || value_2 || value_3)
+    return true;
+  
+  return false;  
+}
+
+
+// returns an array with boolean value for each password.
+// true means that the element at that position did not repeat in the array.
+// false means that element at that index is more than one time in that array.
+function num_times_element(arr)
+{
+	let store=0;
+  var first_time= Array(arr.length).fill(true);
+	for(let i=0;i<arr.length-1;i++)
+	{
+		store=arr[i];
+		for(let j=i+1;j<arr.length;j++)
+		{
+			if(arr[j]===store)
+				first_time[j]=false;
+		}
+	}
+	return first_time;
+}
+
+function validatePasswords(passwords)
+ {
+  isFirstTime=num_times_element(passwords); // contains boolean value for each password, true means the password is not repeated more than once, otherwise it repeats. 
+
+   final_ans=[]; // will contain the final answer(Booleans) for each element
+   for(let i=0;i<passwords.length;i++)
+   {
+     final_ans[i]=check(passwords[i],passwords,isFirstTime[i]);
+
+   } 
+   return final_ans;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
@@ -67,3 +146,5 @@ test(
       validatePasswords(passwords2), [true, true, false, false, false]
    )
  );
+
+ 
