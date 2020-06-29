7c5cc6728b23f1d5cccfcf017fbcc74f2e30ad66
davibaweja <davibaweja@gmail.com>
2020-06-29 10:40:54 +0100

    V3.1 week3
    Completed the tasks of file 1-oxygen-levels.js

--- week-3/2-mandatory/1-oxygen-levels.js
+++ week-3/2-mandatory/1-oxygen-levels.js
@@ -9,8 +9,24 @@ To be safe, they need to land on the first unamed planet that has Oxygen levels 
 Write a function that finds the oxygen level of the first safe planet - Oxygen between 19.5% and 23.5%
 */
 
-function safeLevels() {
+var first=true;
 
+function check(element)
+{
+  if( parseFloat(element) > 19.5 && parseFloat(element) < 23.5 && first )
+  {
+    console.log("first element found is "+element);
+    first=false;
+    return element; 
+  }
+}
+
+function safeLevels(arr) {
+
+  first=true;
+  value=arr.filter(check);
+  final_value= String(value);
+  return final_value;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
@@ -30,7 +46,7 @@ function test(test_name, expr) {
 }
 
 test(
-    "safeLevels function works - case 2",
+    "safeLevels function works - case 1",
     safeLevels(oxygenLevels1) === "19.9%"
 );
 
