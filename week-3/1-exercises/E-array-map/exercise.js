48e6fdf224964c2c99450a98691474a9fbb673bc
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-06-30 14:37:31 +0100

    Update exercise.js
    e-array-map is done

--- week-3/1-exercises/E-array-map/exercise.js
+++ week-3/1-exercises/E-array-map/exercise.js
@@ -3,3 +3,31 @@
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
+function multipleWith100 (number){
+    return number *100;
+}
+
+var multiple100NewArray= numbers.map(multipleWith100);
+/*01
+var multiple100NewArray =numbers.map(function multipleWith100 (number){
+    return number * 100;
+});
+*/
+
+/*02
+var multiple100NewArray =numbers.map(function (number){
+    return number * 100;
+});
+*/
+
+/*03
+var multiple100NewArray =numbers.map( number => {
+    return number * 100;
+});
+*/
+
+/*04
+var multiple100NewArray =numbers.map(number => number * 100);
+*/
+
+console.log (multiple100NewArray);
