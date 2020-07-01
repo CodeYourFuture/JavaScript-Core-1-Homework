40357ec5632333ae50f35abd08a2700785df5832
zubeda <zubedauk@gmail.com>
2020-07-01 11:58:20 +0100

    done

--- week-3/1-exercises/F-array-forEach/exercise.js
+++ week-3/1-exercises/F-array-forEach/exercise.js
@@ -9,6 +9,20 @@
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+arr.forEach(function test(num,index){
+  if(num%3===0 && num%5===0){
+    console.log("FizzBuss");
+  }
+  else if(num%3===0){
+    console.log("Fizz");
+  }else if(num%5===0){
+    console.log("Buzz");
+  }
+  else{
+    console.log(index);
+  }
+});
+
 /* EXPECTED OUTPUT */
 
 /*
