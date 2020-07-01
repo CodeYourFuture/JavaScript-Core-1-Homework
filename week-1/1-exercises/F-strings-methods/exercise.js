238111fa00170ca0aea90068beb39c41d2582395
Ali Haider <alihaiderx736@gmail.com>
2020-07-01 11:08:49 +0100

    F : exercise and exercise2 is completed

--- week-1/1-exercises/F-strings-methods/exercise.js
+++ week-1/1-exercises/F-strings-methods/exercise.js
@@ -1,3 +1,9 @@
 // Start by creating a variable `message`
+var name = "Ali Haider";
+var nameLength = name.length;
+var greeting = "My name is ";
+var greeting2 = " and my name is ";
+var message = " characters long";
+var totalMessage = greeting + name + greeting2+ nameLength + message;
+console.log(totalMessage);
 
-console.log(message);
--- week-1/1-exercises/F-strings-methods/exercise2.js
+++ week-1/1-exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,8 @@
-const name = " Daniel  ";
-
-console.log(message);
+const name = "   Ali Haider    ";
+var orignalName = name.trim();
+var orignalNameLength = orignalName.length;
+var greeting = "My name is ";
+var greeting2 = " and my name is ";
+var message = " characters long";
+var totalMessage = greeting + orignalName + greeting2 + orignalNameLength+ message;
+console.log(totalMessage);
