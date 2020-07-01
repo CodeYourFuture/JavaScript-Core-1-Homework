03eb52b893b71a4143e6fc25658b3a352fbc156a
Osagie Okoedo <okoedooc@yahoo.com>
2020-07-01 17:10:24 +0100

    learnt map method and used parseFloat

--- week-3/2-mandatory/1-oxygen-levels.js
+++ week-3/2-mandatory/1-oxygen-levels.js
@@ -9,14 +9,14 @@ To be safe, they need to land on the first unamed planet that has Oxygen levels 
 Write a function that finds the oxygen level of the first safe planet - Oxygen between 19.5% and 23.5%
 */
 
-function safeLevels() {
+function safeLevels(arr) {
     const convert = arr.map(function (level) {
       return (parseFloat(level))
     })
     const safeLevels = convert.filter(function (n) {
       return n > 19.5 && n < 23.5
     })
-  return `"${safeLevels[0]}%"`
+  return `${safeLevels[0]}%`
 }
 // let output = `"${safeLevels[0]}%"`
 // console.log(output)
