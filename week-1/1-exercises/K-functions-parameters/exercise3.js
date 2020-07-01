dcfda0eefd2671dbb2aeefc4506455879af0db73
Ali Haider <alihaiderx736@gmail.com>
2020-07-01 12:29:50 +0100

    K : exercise , exercise2 , exercise3, exercise4 and exercise5 completed

--- week-1/1-exercises/K-functions-parameters/exercise3.js
+++ week-1/1-exercises/K-functions-parameters/exercise3.js
@@ -1,5 +1,8 @@
 // Write your function here
-
+function createGreeting(name){
+    var message = "Hello, my name is "+ name;
+    return message;
+}
 var greeting = createGreeting("Daniel");
 
 console.log(greeting);
--- week-1/1-exercises/K-functions-parameters/exercise5.js
+++ week-1/1-exercises/K-functions-parameters/exercise5.js
@@ -1,5 +1,11 @@
 // Declare your function here
-
+function createLongGreeting(name , age){
+    var message = "Hello, my name is ";
+    var message2 = " and I'm ";
+    var message3 = " years old";
+    var finalMessage = message + name + message2+ age + message3;
+    return finalMessage;
+}
 const greeting = createLongGreeting("Daniel", 30);
 
 console.log(greeting);
