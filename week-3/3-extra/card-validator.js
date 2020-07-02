231da57e2402376fbf9e54ca4a1ba4d54364416e
Gennady Tabala <gtabala@gmail.com>
2020-07-02 19:56:53 +0100

    card validator.

--- week-3/3-extra/card-validator.js
+++ week-3/3-extra/card-validator.js
@@ -10,19 +10,22 @@ function isBankCardValid(cardNumber){
     secondGroup = cardNumber.slice(4, 8);
     thirdGroup = cardNumber.slice(8, 12);
     fourthGroup = cardNumber.slice(12);
-    if (numberArray.every(number => typeof number === "number")){
-         return `The bank card number ${firstGroup} ${secondGroup} ${thirdGroup} ${fourthGroup} is not valid.`; 
+    if (numberArray.length !== 16){
+        return `The bank card number ${firstGroup} ${secondGroup} ${thirdGroup} ${fourthGroup} is not valid.length`;    
     }
-    if(numberArray.reduce((accumulator, number) => accumulator + number) <= 16){
-        return `The bank card number ${firstGroup} ${secondGroup} ${thirdGroup} ${fourthGroup} is not valid.`; 
+    if (numberArray.some(number =>  parseInt(number)+"" === "")){
+         return `The bank card number ${firstGroup} ${secondGroup} ${thirdGroup} ${fourthGroup} is not valid.nn`; 
+    }
+    if(numberArray.reduce((accumulator, number) => accumulator + parseInt(number)) <= 16){
+        return `The bank card number ${firstGroup} ${secondGroup} ${thirdGroup} ${fourthGroup} is not valid 16.`; 
     }
     if(numberArray[15] % 2 === 1){
-        return `The bank card number ${firstGroup} ${secondGroup} ${thirdGroup} ${fourthGroup} is not valid.`; 
+        return `The bank card number ${firstGroup} ${secondGroup} ${thirdGroup} ${fourthGroup} is not valid. even`; 
     }
     if(numberArray.every((number, index, arr) => number === numberArray[0])){
-        return `The bank card number ${firstGroup} ${secondGroup} ${thirdGroup} ${fourthGroup} is not valid.`; 
+        return `The bank card number ${firstGroup} ${secondGroup} ${thirdGroup} ${fourthGroup} is not valid.9`; 
     }
     return `The bank card number ${firstGroup} ${secondGroup} ${thirdGroup} ${fourthGroup} is valid.`;
    
 }
-console.log(isBankCardValid("00000000000000b7"));
+console.log(isBankCardValid("888888888888n808"));
