b1bbb6a64da3ce92e177b33a9a731606ee151790
Abdennour Hachemi <nouri89@hotmail.com>
2020-07-04 00:46:03 +0100

    solved

--- week-3/1-exercises/B-array-some/exercise.js
+++ week-3/1-exercises/B-array-some/exercise.js
@@ -5,20 +5,26 @@
   - Add a check for null values, and if one exists, exit the program
   - Do not edit any of the existing code
 */
-
-var pairsByIndex = [[0, 3], [1, 2], [2, 1], null, [3, 0]];
+                                               
+var pairsByIndex = [ [ 0, 3 ], [ 1, 2 ], [ 2, 1 ], null, [ 3, 0 ] ];
 
 // If there is a null value in the array exit the program with the error code
 // https://nodejs.org/api/process.html#process_process_exit_code
 // process.exit(1);
 
-var students = ["Islam", "Lesley", "Harun", "Rukmini"];
-var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
+var students = [ 'Islam', 'Lesley', 'Harun', 'Rukmini' ];
+var mentors = [ 'Daniel', 'Irina', 'Mozafar', 'Luke' ];
 
 var pairs = pairsByIndex.map(function(indexes) {
-  var student = students[indexes[0]];
-  var mentor = mentors[indexes[1]];
-  return [student, mentor];
+	if (indexes === null) {
+		console.log('Error: A null value found');
+		process.exit(1);
+		
+	}
+	var student = students[indexes[0]];
+	var mentor = mentors[indexes[1]];
+	return [ student, mentor ];
 });
 
 console.log(pairs);
+                                                       
