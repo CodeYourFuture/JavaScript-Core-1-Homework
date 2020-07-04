9b64ef379f4083777a76a8506be249584922f413
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-07-04 23:02:16 +0100

    Update exercise.js
    d-array-filter.js is done

--- week-3/1-exercises/D-array-filter/exercise.js
+++ week-3/1-exercises/D-array-filter/exercise.js
@@ -11,9 +11,8 @@ var pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
 var pairsByIndex = pairsByIndexRaw.filter(filterOut); ; // Complete this statement
 
 function filterOut(index){
-
-  let notFiltered = index.filter(a => a === "null" && a === "false" && a === "string");
-  return index.length > 1;
+  //return Array.isArray(index) && index.length == 2;
+  return index instanceof Array && index.length == 2
 }
 
 var students = ["Islam", "Lesley", "Harun", "Rukmini"];
