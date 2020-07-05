71051f58a194b3caf1a44d1912ea8ed543fed95c
Abdennour Hachemi <nouri89@hotmail.com>
2020-07-06 00:57:36 +0100

    solved

--- week-3/2-mandatory/2-bush-berries.js
+++ week-3/2-mandatory/2-bush-berries.js
@@ -10,25 +10,33 @@
   Use the tests to confirm which message to return
 */
 
-function bushChecker() {
+function bushChecker(arr) {
+	var test1 = arr.every(function(item) {
+		return item === 'pink';
+	});
 
+	if (test1 === true) {
+		return 'Bush is safe to eat from';
+	} else {
+		return 'Toxic! Leave bush alone!';
+	}
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
-let bushBerryColours1 = ["pink", "pink", "pink", "neon", "pink", "transparent"]
-let bushBerryColours2 = ["pink", "pink", "pink", "pink"]
+let bushBerryColours1 = [ 'pink', 'pink', 'pink', 'neon', 'pink', 'transparent' ];
+let bushBerryColours2 = [ 'pink', 'pink', 'pink', 'pink' ];
 
 function test(test_name, expr) {
-  let status;
-  if (expr) {
-    status = "PASSED";
-  } else {
-    status = "FAILED";
-  }
+	let status;
+	if (expr) {
+		status = 'PASSED';
+	} else {
+		status = 'FAILED';
+	}
 
-  console.log(`${test_name}: ${status}`);
+	console.log(`${test_name}: ${status}`);
 }
 
-test("bushChecker funtion works - case 1", bushChecker(bushBerryColours1) === "Toxic! Leave bush alone!")
-test("bushChecker funtion works - case 1", bushChecker(bushBerryColours2) === "Bush is safe to eat from")
+test('bushChecker funtion works - case 1', bushChecker(bushBerryColours1) === 'Toxic! Leave bush alone!');
+test('bushChecker funtion works - case 1', bushChecker(bushBerryColours2) === 'Bush is safe to eat from');
