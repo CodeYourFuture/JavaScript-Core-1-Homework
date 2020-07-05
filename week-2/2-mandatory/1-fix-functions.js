5d30745d24a3857e0f5207596cad97043f0483c9
Ali Haider <alihaiderx736@gmail.com>
2020-07-05 17:17:50 +0100

    Mandatory Exercises completed.

--- week-2/2-mandatory/1-fix-functions.js
+++ week-2/2-mandatory/1-fix-functions.js
@@ -1,20 +1,20 @@
 // The below functions are syntactically correct but not outputting the right results.
 // Look at the tests and see how you can fix them.
 
-function mood() {
-  let isHappy = true;
-
-  if (isHappy) {
-    return "I am happy";
-  } else {
+function mood(str) {
+  if(str == true){
+  return "I am happy";
+  }
+  else {
     return "I am not happy";
   }
+
 }
 
 function greaterThan10(num) {
-  let isBigEnough;
+  let isBigEnough = num;
 
-  if (isBigEnough) {
+  if (isBigEnough > 10) {
     return "num is greater than 10";
   } else {
     return "num is not big enough";
@@ -22,21 +22,18 @@ function greaterThan10(num) {
 }
 
 function sortArray(letters) {
-  let sortedLetters = letters;
-
+  let sortedLetters = letters.sort();
   return sortedLetters;
 }
 
 function first5(numbers) {
-  let sliced;
-
+  let sliced = numbers.slice(0,5);
   return sliced;
 }
 
 function get3rdIndex(arr) {
   let index = 3;
-  let element;
-
+  let element = arr[index];
   return element;
 }
 
