b92d1ec1564dfb8c5767fc4013372afc75d5aede
Ali Haider <alihaiderx736@gmail.com>
2020-07-05 15:21:48 +0100

    F- logical operators exercise2 completed.

--- week-2/1-exercises/F-logical-operators/exercise2.js
+++ week-2/1-exercises/F-logical-operators/exercise2.js
@@ -5,7 +5,41 @@
   Update the code so that you get the expected result.
 */
 
-function isNegative() {}
+function isNegative(number) {
+  if ( number < 0){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+  function isBetween5and10( number){
+    if (number >5  && number<= 10){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  function isShortName(name) {
+     if ( name.length <7){
+       return true;
+     }
+     else {
+       return false;
+     }
+  }
+  function startsWithD(name){
+    if(name [0] === "D")
+    {
+      return true;
+
+    }
+    else {
+      return false;
+    }
+  }
 
 /* 
   DO NOT EDIT BELOW THIS LINE
