734d489461c3aeda4969e6c91ec1aa2432575cc0
osagiestar <okoedooc@yahoo.com>
2020-07-06 21:44:50 +0100

    cleaned up code

--- week-3/2-mandatory/1-oxygen-levels.js
+++ week-3/2-mandatory/1-oxygen-levels.js
@@ -18,8 +18,6 @@ function safeLevels(arr) {
   });
   return `${safeLevels[0]}%`;
 }
-// let output = `"${safeLevels[0]}%"`
-// console.log(output)
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
@@ -27,33 +25,23 @@ const oxygenLevels1 = ["24.2%", "11.3%", "19.9%", "23.1%", "29.3%", "20.2%"];
 const oxygenLevels2 = ["30.8%", "23.5%", "18.8%", "19.5%", "20.2%", "31.6%"];
 const oxygenLevels3 = ["200%", "21.1%"];
 
-const util = require('util');
+const util = require("util");
 
 function test(test_name, actual, expected) {
   let status;
   if (actual === expected) {
     status = "PASSED";
   } else {
-    status = `FAILED: expected: ${util.inspect(expected)} but your function returned: ${util.inspect(actual)}`;
+    status = `FAILED: expected: ${util.inspect(
+      expected
+    )} but your function returned: ${util.inspect(actual)}`;
   }
 
   console.log(`${test_name}: ${status}`);
 }
 
-test(
-  "safeLevels function works - case 1",
-  safeLevels(oxygenLevels1),
-  "19.9%"
-);
+test("safeLevels function works - case 1", safeLevels(oxygenLevels1), "19.9%");
 
-test(
-  "safeLevels function works - case 2",
-  safeLevels(oxygenLevels2),
-  "20.2%"
-);
+test("safeLevels function works - case 2", safeLevels(oxygenLevels2), "20.2%");
 
-test(
-  "safeLevels function works - case 3",
-  safeLevels(oxygenLevels3),
-  "21.1%"
-);
+test("safeLevels function works - case 3", safeLevels(oxygenLevels3), "21.1%");
