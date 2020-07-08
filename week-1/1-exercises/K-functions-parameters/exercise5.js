d5a93121122f3c769b943c80dc84d39872de1cce
osagiestar <okoedooc@yahoo.com>
2020-07-08 03:08:42 +0100

    debugging codes

--- week-1/1-exercises/K-functions-parameters/exercise5.js
+++ week-1/1-exercises/K-functions-parameters/exercise5.js
@@ -1,8 +1,8 @@
 // Declare your function here
-function createLongGreeting() {
-  let name = "Daniel";
-  let age = 30;
+function createLongGreeting(name, age) {
+  // let name = "Daniel";
+  // let age = 30;
   const greeting = `My name is ${name} and I'm ${age} years old`;
   return greeting;
 }
-console.log(createLongGreeting());
+console.log(createLongGreeting("Daniel", 30));
