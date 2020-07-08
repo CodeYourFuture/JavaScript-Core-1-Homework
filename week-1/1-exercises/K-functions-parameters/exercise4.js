7ed52af0f149bc6b84431b655952476d3d447273
osagiestar <okoedooc@yahoo.com>
2020-07-08 02:56:18 +0100

    debugging based on feedback

--- week-1/1-exercises/K-functions-parameters/exercise4.js
+++ week-1/1-exercises/K-functions-parameters/exercise4.js
@@ -1,11 +1,6 @@
 // Declare your function first
-function sum() {
-  return 13 + 124;
-  //   let num1 = 13;
-  //   let num2 = 124;
-  //   let addition = 13 + 124;
-  //   return addition;
+function sum(num1, num2) {
+  return num1 + num2;
 }
 // Call the function and assign to a variable `sum`
-
-console.log(sum());
+console.log(sum(13, 124)); // calling the function and passing the numbers
