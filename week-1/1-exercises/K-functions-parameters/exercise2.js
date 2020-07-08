2983e664bd05de886cf7e4ebdb6417d6a3aadb45
osagiestar <okoedooc@yahoo.com>
2020-07-08 02:29:41 +0100

    debugging from feedback

--- week-1/1-exercises/K-functions-parameters/exercise2.js
+++ week-1/1-exercises/K-functions-parameters/exercise2.js
@@ -1,7 +1,7 @@
 // Declare your function first
-function divide() {
-  return 3 / 4;
+function divide(a, b) {
+  return a / b;
 }
-let result = divide();
+let result = divide(3, 4);
 
 console.log(result);
