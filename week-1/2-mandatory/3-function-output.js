0bf0f1f4b68b9f29b67e63be6f1b17b18e82dc8c
osagiestar <okoedooc@yahoo.com>
2020-07-10 12:06:27 +0100

    debugging

--- week-1/2-mandatory/3-function-output.js
+++ week-1/2-mandatory/3-function-output.js
@@ -1,7 +1,7 @@
 // Add comments to explain what this function does. You're meant to use Google!
 function letterSet() {
   return Math.random() * 10; //Math.random() syntax is a floating-point, pseudo-random number between 0 (inclusive) and 1 (exclusive).
-}
+} //This will return random numbers between 0 to 9: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9.
 
 // Add comments to explain what this function does. You're meant to use Google!
 function words(w1, w2, w3) {
@@ -10,15 +10,10 @@ function words(w1, w2, w3) {
 /*Option 2: return w1.concat(w2, w3);*/
 
 function concatenate(firstWord, secondWord, thirdWord) {
-<<<<<<< HEAD
   // Write the body of this function to concatenate three words together
   // Look at the test case below to understand what to expect in return
 
   return `${firstWord} ${secondWord} ${thirdWord}`;
-=======
-  // Write the body of this function to concatenate three words together.
-  // Look at the test case below to understand what this function is expected to return.
->>>>>>> 8d17c19782cb831f7eeeb6c2adc0555b17d81af4
 }
 
 /* ======= TESTS - DO NOT MODIFY =====
@@ -26,31 +21,33 @@ There are some Tests in this file that will help you work out if your code is wo
 To run these tests type `node 3-function-output` into your terminal
 */
 
-const util = require('util');
+const util = require("util");
 
 function test(test_name, actual, expected) {
-    let status;
-    if (actual === expected) {
-        status = "PASSED";
-    } else {
-        status = `FAILED: expected: ${util.inspect(expected)} but your function returned: ${util.inspect(actual)}`;
-    }
+  let status;
+  if (actual === expected) {
+    status = "PASSED";
+  } else {
+    status = `FAILED: expected: ${util.inspect(
+      expected
+    )} but your function returned: ${util.inspect(actual)}`;
+  }
 
-    console.log(`${test_name}: ${status}`);
+  console.log(`${test_name}: ${status}`);
 }
 
 test(
   "concatenate function - case 1 works",
-  concatenate('code', 'your', 'future'),
+  concatenate("code", "your", "future"),
   "code your future"
 );
 test(
   "concatenate function - case 2 works",
-  concatenate('I', 'like', 'pizza'),
+  concatenate("I", "like", "pizza"),
   "I like pizza"
 );
 test(
   "concatenate function - case 3 works",
-  concatenate('I', 'am', 13),
+  concatenate("I", "am", 13),
   "I am 13"
 );
