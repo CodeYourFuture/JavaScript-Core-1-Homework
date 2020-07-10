4a7a7a156bf630b5855f029fa518e0bdfe13dec0
osagiestar <okoedooc@yahoo.com>
2020-07-10 12:04:56 +0100

    debugging

--- week-3/2-mandatory/6-lane-names.js
+++ week-3/2-mandatory/6-lane-names.js
@@ -21,49 +21,22 @@ const streetNames = [
   "Alban Highwalk",
 ];
 
-<<<<<<< HEAD
-function arraysEqual(a, b) {
-  if (a === b) return true;
-  if (a == null || b == null) return false;
-  if (a.length != b.length) return false;
+const util = require("util");
 
-  for (let i = 0; i < a.length; ++i) {
-    if (a[i] !== b[i]) return false;
-  }
-
-  return true;
-}
-
-function test(test_name, expr) {
+function test(test_name, actual, expected) {
   let status;
-  if (expr) {
+  if (util.isDeepStrictEqual(actual, expected)) {
     status = "PASSED";
   } else {
-    status = "FAILED";
+    status = `FAILED: expected: ${util.inspect(
+      expected
+    )} but your function returned: ${util.inspect(actual)}`;
   }
 
   console.log(`${test_name}: ${status}`);
-=======
-const util = require('util');
-
-function test(test_name, actual, expected) {
-    let status;
-    if (util.isDeepStrictEqual(actual, expected)) {
-        status = "PASSED";
-    } else {
-        status = `FAILED: expected: ${util.inspect(expected)} but your function returned: ${util.inspect(actual)}`;
-    }
-
-    console.log(`${test_name}: ${status}`);
->>>>>>> 8d17c19782cb831f7eeeb6c2adc0555b17d81af4
 }
 
-test(
-  "getLanes function works",
-<<<<<<< HEAD
-  arraysEqual(getLanes(streetNames), ["Abchurch Lane", "Addle Lane"])
-=======
-  getLanes(streetNames),
-  ["Abchurch Lane", "Addle Lane"]
->>>>>>> 8d17c19782cb831f7eeeb6c2adc0555b17d81af4
-);
+test("getLanes function works", getLanes(streetNames), [
+  "Abchurch Lane",
+  "Addle Lane",
+]);
