52dc1fc930d3e56d02c3e354b3e6b2fec154cc95
Ali Haider <alihaiderx736@gmail.com>
2020-07-15 17:50:12 +0100

    C-array-every exercise completed

--- week-3/1-exercises/C-array-every/exercise.js
+++ week-3/1-exercises/C-array-every/exercise.js
@@ -4,9 +4,8 @@
 
 var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
 var group = ["Austine", "Dany", "Swathi", "Daniel"];
-
-var groupIsOnlyStudents; // complete this statement
-
+var groupIsOnlyStudents = group.every(x => students.includes(x));// complete this statement
+     
 if (groupIsOnlyStudents) {
   console.log("The group contains only students");
 } else {
